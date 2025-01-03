4dfe012045cbc77667bde9b5af29dd176d506e97
obwoj1 <149099282+obwoj1@users.noreply.github.com>
2025-01-03 13:30:43 -0500

    Initial Commit

---
+++ InvestorIN(1)Tests/InvestorIN_1_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  InvestorIN_1_Tests.swift
+//  InvestorIN(1)Tests
+//
+//  Created by Oba W on 1/3/25.
+//
+
+import Testing
+@testable import InvestorIN_1_
+
+struct InvestorIN_1_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
